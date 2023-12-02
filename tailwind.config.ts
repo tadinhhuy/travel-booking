315bbf7d94598f761ffb69287de550b3f7c3d3ca
HuyTa <tadinhhuy907@gmail.com>
2023-12-02 22:33:29 +0700

    feat: initialize configure project

---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from 'tailwindcss';
+import theme from './theme.config';
+
+/** @type {import('tailwindcss').Config} */
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        ...theme.colors,
+        'neutral-650': '#424955FF',
+        'neutral-900': '#171A1FFF',
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
