f6165250690b430ea630277ff295912f4766407c
Huy <tadinhhuy907@gmail.com>
2023-12-30 00:46:44 +0700

    feat: generate search result layout

---
+++ src/app/search-results/layout.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Search Results Page',
+  description: 'Travel Booking - Search Results Page',
+};
+
+export default function SearchResultLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
