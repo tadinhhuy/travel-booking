1c6cd30c80d98f1afbdbb97f8967b67fb7d2bfe2
Huy <tadinhhuy907@gmail.com>
2024-05-20 23:58:33 +0700

    Renamed header folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Montserrat } from 'next/font/google';
 import './globals.css';
-import Header from '@/components/Header';
+import Header from '@/components/header';
 import Footer from '@/components/footer';
 
 const inter = Montserrat({ subsets: ['latin'] });
